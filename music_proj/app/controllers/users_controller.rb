005b0602c03cdaf7a9b7a61ea3ea2788e5c9f8d7
Kaiter Wu <wukaiter@gmail.com>
2023-02-21 14:27:03 -0500

    creating methods in controllers

--- music_proj/app/controllers/users_controller.rb
+++ music_proj/app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
+    def new 
+        @user = User.new
+        render: new 
+    end 
+
     def create 
-        
+        @user = User.new(params[user_params])
+        if @user.save
+            login(@user)
+            redirect_to user_url(@user)
+        else  
+           render: new 
+        end 
+    end 
+
+    def show 
+        @user = User.find(params[:id])
+        render: show 
+    end 
+
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:email,:password)
     end 
 end 
